0e78fa72c99ee03e64d41ed9631f3472d0181c0d
morelucks <luckykamshak@gmail.com>
2025-02-06 22:28:38 +0100

    modification

--- studentAtendance.go
+++ studentAtendance.go
@@ -12,13 +12,40 @@ func (s Student)addStudent(studentName string, studentAge int){
 	s.age = uint8(studentAge)
 }
 
+func (s *Student)DeleteStudent(){
+	s.name=""
+	s.age=0
+}
+
+
+func (s Student)UdateStudent(NewName string, newAge int){
+	s.name=NewName
+	s.age=uint8(newAge)
+
+}
+
 func main(){
 	student1:= Student{
 		name:"luckify",
 		age: 98,
 	}
 
-	student1.addStudent(student1.name, int(student1.age))
-	fmt.Printf("student %v \n", student1.name)
 
+	student2:= Student{
+		name:"luc",
+		age: 98,
+	}
+	student2.addStudent(student2.name, int(student2.age))
+
+	student1.addStudent(student1.name, int(student1.age))
+
+	fmt.Println("before")
+	fmt.Printf("student %v \n", student1.name)
+	fmt.Printf("student %v \n", student2.name)
+
+	student1.DeleteStudent()
+	fmt.Println("after")
+	// student1.UdateStudent("grace", 4)
+	fmt.Printf("student %v \n", student1.name)
+	fmt.Printf("student %v \n", student2.name)
 }
